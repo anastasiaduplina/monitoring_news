c66248174856f7e8c3ace52270fad227244cd780
anastasiaduplina <anastasiaduplina@yandex.ru>
2023-04-16 00:55:24 +0300

    docker done) and add some controller

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dto.AddNetwork;
 import org.example.dto.AddUser;
+import org.example.dto.FindNews;
+import org.example.model.News;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/monitoring")
@@ -18,14 +21,19 @@ import javax.validation.Valid;
 public class Controller {
 	@Autowired
 	private UserService userService;
-	//users
-	@GetMapping("/addUser")
-	public String addUser(//@RequestBody @Valid AddUser addUser){}
-	){
-		return "OK";
-	}
 
-	//networks
+	//чуть позже добавлю spring security
+	@PostMapping("/addUser")
+	public void addUser(@RequestBody @Valid AddUser addUser){}
+	@GetMapping("/getNews/filter")
+	public List<News> getNewsByFilter(@RequestBody @Valid FindNews findNews){
+		return null;
+	}
+	@PutMapping("/addToFavourite")
+	public  void addToFavourite(@RequestBody Long id_news){}
 	@PostMapping("/addNetwork")
 	public void addNetwork(@RequestBody @Valid AddNetwork addNetwork){}
+
+	@GetMapping("/getPopularNews")
+	public void getPopularNews(){}
 }
---
+++ src/main/java/org/example/dto/FindNews.java
@@ -0,0 +1,18 @@
+package org.example.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+@Data
+public class FindNews {
+	@NotBlank
+	String keyWord;
+	@Max(100)
+	Integer count;
+	String network;
+	Timestamp dateFrom;
+	Timestamp dateTo;
+}
