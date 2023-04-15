af82daf8f127ae8bf7fb02339d24e3da0da691e4
anastasiaduplina <anastasiaduplina@yandex.ru>
2023-04-15 18:52:16 +0300

    add swagger

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -1,4 +1,31 @@
 package org.example.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.dto.AddNetwork;
+import org.example.dto.AddUser;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/monitoring")
+@Validated
+@Slf4j
 public class Controller {
+	@Autowired
+	private UserService userService;
+	//users
+	@GetMapping("/addUser")
+	public String addUser(//@RequestBody @Valid AddUser addUser){}
+	){
+		return "OK";
+	}
+
+	//networks
+	@PostMapping("/addNetwork")
+	public void addNetwork(@RequestBody @Valid AddNetwork addNetwork){}
 }
---
+++ src/main/java/org/example/dto/AddUser.java
@@ -0,0 +1,13 @@
+package org.example.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class AddUser {
+	@NotBlank
+	String login;
+	@NotBlank
+	String password;
+}
